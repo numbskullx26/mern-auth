e84fcc019b4219781574c51f58bac3b6269425a7
Indrajeet Sarkar <sarkarraj1978@gmail.com>
2023-11-29 01:27:45 +0530

    add : sign in page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { set } from "mongoose";
 
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 
 export default function SignUp() {
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
@@ -31,6 +31,7 @@ export default function SignUp() {
         setError(true);
         return;
       }
+      navigate("/sign-in");
     } catch (error) {
       setLoading(false);
       setError(true);
